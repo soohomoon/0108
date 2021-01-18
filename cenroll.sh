76c64ac689d292abbfddf595696f534cd813f05e
soohomoon <demowin@gmail.com>
2021-01-18 18:30:53 +0900

    Update cenroll.sh

--- cenroll.sh
+++ cenroll.sh
@@ -61,38 +61,7 @@ function generate_computer_name()
     return 0
 }
 
-function vault()
-{
-	    
-	VAULTED_ACCOUNTS=local-manager,local-user
-	LOGIN_ROLES="System Administrator"
-	echo "post hook script started." >> /var/centrify/tmp/vaultaccount.log
-	Permissions=()
-	Field_Separator=$IFS
-	IFS=","
-	read -a roles <<< $LOGIN_ROLES
-	IFS=
-	for role in ${roles[@]} 
-	  do 
-	     Permissions=("${Permissions[@]}" "-p" "\"role:$role:View,Login,Checkout\"" )
-	done
-	IFS=","
-	sleep 10
-	for account in $VAULTED_ACCOUNTS; do
-	   PASS=`openssl rand -base64 20`
-	   if id -u $account > /dev/null 2>&1; then
-	      echo $PASS | passwd --stdin $account
-	   else
-	      useradd -m $account -g sys
-	      echo $PASS | passwd --stdin $account
-	   fi
-	   IFS=
-	   echo "Vaulting password for $account" >> /tmp/vaultaccount.log 2>&1
-	   echo $PASS | /usr/sbin/csetaccount -V --stdin -m true ${Permissions[@]} $account >> /tmp/vaultaccount.log 2>&1
-	done
-	IFS=$Field_Separator
 
-}
 
 prepare_for_cenroll
 r=$?
@@ -126,4 +95,3 @@ done
     #"${CMDPARAMARRAY[@]}"
     
     
-    vault
