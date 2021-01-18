df335179e594dbbad6f23020f940567360141ebe
soohomoon <demowin@gmail.com>
2021-01-18 17:36:00 +0900

    Update adjoin.sh

--- adjoin.sh
+++ adjoin.sh
@@ -67,6 +67,39 @@ function do_adedit()
     fi
 }
 
+function vault()
+{
+	    
+	VAULTED_ACCOUNTS=local-manager,local-user
+	LOGIN_ROLES="System Administrator"
+	echo "post hook script started." >> /var/centrify/tmp/vaultaccount.log
+	Permissions=()
+	Field_Separator=$IFS
+	IFS=","
+	read -a roles <<< $LOGIN_ROLES
+	IFS=
+	for role in ${roles[@]} 
+	  do 
+	     Permissions=("${Permissions[@]}" "-p" "\"role:$role:View,Login,Checkout\"" )
+	done
+	IFS=","
+	sleep 10
+	for account in $VAULTED_ACCOUNTS; do
+	   PASS=`openssl rand -base64 20`
+	   if id -u $account > /dev/null 2>&1; then
+	      echo $PASS | passwd --stdin $account
+	   else
+	      useradd -m $account -g sys
+	      echo $PASS | passwd --stdin $account
+	   fi
+	   IFS=
+	   echo "Vaulting password for $account" >> /var/centrify/tmp/vaultaccount.log 2>&1
+	   echo $PASS | /usr/sbin/csetaccount -V --stdin -m true ${Permissions[@]} $account >> /var/centrify/tmp/vaultaccount.log 2>&1
+	done
+	IFS=$Field_Separator
+
+}
+
 private_ip=`curl --fail -s http://169.254.169.254/latest/meta-data/local-ipv4`
 host_name="`echo $private_ip | sed -n 's/\./-/gp'`"
 
@@ -80,4 +113,5 @@ host_name="`echo $private_ip | sed -n 's/\./-/gp'`"
 
 /usr/sbin/adjoin $DOMAIN -z "$ZONE" --name '$host_name' -E /var/prestage_cache $ADDITIONAL_OPS
 
+vault
 #do_adedit
