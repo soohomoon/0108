e6e6c68b66c8e67de523f0693146253d37a0afbd
soohomoon <demowin@gmail.com>
2021-01-19 15:55:18 +0900

    Update adjoin.sh

--- adjoin.sh
+++ adjoin.sh
@@ -114,8 +114,8 @@ host_name="`echo $private_ip | sed -n 's/\./-/gp'`"
 #vault
 /usr/sbin/adjoin $DOMAIN -z "$ZONE" --name "$host_name" -E /var/prestage_cache $ADDITIONAL_OPS
 
-cedit --set cli.hook.cenroll:/tmp/auto_centrify_deployment/centrifycc/vaultaccount.sh
-source /tmp/auto_centrify_deployment/centrifycc/vaultaccount.sh
+#cedit --set cli.hook.cenroll:/tmp/auto_centrify_deployment/centrifycc/vaultaccount.sh
+#source /tmp/auto_centrify_deployment/centrifycc/vaultaccount.sh
 
 
 #do_adedit
